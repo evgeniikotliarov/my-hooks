4dd47bdf379ef4b3ccab93f044022d1e4df652c9
evgenii <evk@iopent.ru>
2021-05-27 18:06:01 +0300

    use-state-hook

--- my-hooks/src/App.tsx
+++ my-hooks/src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
+function initialState() {
+  return Math.trunc(Math.random() * 150)
+}
 
 function App() {
+  const [counter, setCounter] = useState(() => {
+    return initialState();
+  })
+
+  function increment() {
+    setCounter(prev => prev + 1)
+  }
+
+  function decrement() {
+    setCounter(counter - 1)
+  }
+
+  const [state, setState] = useState({
+    title: "Counter",
+    date: Date.now()
+  })
+
+  function updateTitle() {
+    setState(prevState => {
+      return {
+        ...prevState,
+        title: "New counter"
+      }
+    })
+  }
+
+  const {title} = state;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={"container"}>
+        <h1>{title}: {counter}</h1>
+        <button onClick={increment} className="btn btn-primary">Добавить</button>
+        <button onClick={decrement} className="btn btn-danger">Убрать</button>
+        <button onClick={updateTitle} className="btn btn-default">Изменить</button>
+
+        <pre>{JSON.stringify(state, null, 2)}</pre>
+      </div>
     </div>
   );
 }
